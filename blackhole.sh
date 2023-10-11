e73676f175dd8794569bc75b2093a4b8552df7ba
Asentrixx <venomstudios1@gmail.com>
2023-10-11 18:51:31 +1000

    Update blackhole.sh

--- blackhole.sh
+++ blackhole.sh
@@ -4,7 +4,13 @@
 declare -A URLS=(
 ["Google"]="https://asn.ipinfo.app/api/text/ipblackhole/AS15169"
 ["ESET"]="https://asn.ipinfo.app/api/text/ipblackhole/AS50881"
-["Another"]="https://asn.ipinfo.app/api/text/ipblackhole/AS198605"
+["AVAST"]="https://asn.ipinfo.app/api/text/ipblackhole/AS198605"
+["Sophos"]="https://asn.ipinfo.app/api/text/ipblackhole/AS58976"
+["Sophos"]="https://asn.ipinfo.app/api/text/ipblackhole/AS399342"
+["DigitalOcean"]="https://asn.ipinfo.app/api/text/ipblackhole/AS14061"
+["BlixSolutions"]="https://asn.ipinfo.app/api/text/ipblackhole/AS50304"
+["Hetzner"]="https://asn.ipinfo.app/api/text/ipblackhole/AS24940"
+["KeliWeb SRL"]="https://asn.ipinfo.app/api/text/ipblackhole/AS202675"
 )
 
 # Loop through array
